e3c4122943ee3cf06cc32289879849d700195b47
Danneyl Serna <114704164+AlterEgoSE@users.noreply.github.com>
2022-10-06 23:30:31 -0500

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿// Diseñar un programa para imprimir todos los números primos entre 2 y 100 inclusive.
+
+int num = 0;
+for (int i = 2; i <= 100; i++)
+{
+    for (int j = 1; j <= i; j++)
+    {
+        if (i % j == 0)
+        {
+            num = num + 1;
+        }
+    }    
+    if (num <= 2)
+    {
+        Console.WriteLine(i);
+    }
+    num = 0;
+}
+Console.ReadKey();
+
