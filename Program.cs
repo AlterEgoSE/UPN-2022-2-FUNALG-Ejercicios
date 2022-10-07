6bc3c72a4a9fe71e9222cda9c663596ccd7f1bc7
Danneyl Serna <114704164+AlterEgoSE@users.noreply.github.com>
2022-10-06 23:35:15 -0500

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿/* Diseñar un programa para leer una serie de números hasta obtener un número superior a 100.
+
+int num;
+
+Console.Write("Ingresa un número: ");
+num = int.Parse(Console.ReadLine());
+
+while (num <= 100)
+{
+    Console.WriteLine("Digite de nuevo: ");
+    num = int.Parse(Console.ReadLine());
+}
+*/
+int num;
+
+do
+{
+    Console.Write("Ingresa un número: ");
+    num = int.Parse(Console.ReadLine());
+}
+while (num <=100);
