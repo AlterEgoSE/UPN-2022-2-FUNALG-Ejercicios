a9d4602fa160d00e870ead4d3bf16afabf4132eb
Danneyl Serna <114704164+AlterEgoSE@users.noreply.github.com>
2022-10-06 23:37:23 -0500

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿// Diseñar un programa para leer sucesivamente números del teclado hasta que aparezca
+// un número comprendido entre 1 y 5.;
+
+int num;
+
+Console.Write("Digite un número: ");
+num = int.Parse(Console.ReadLine());
+
+while ( num <2 || num > 4)
+{
+    Console.WriteLine("Digite de nuevo: ");
+    num = int.Parse(Console.ReadLine());
+}
+Console.ReadKey();
