4cd81f56204a7320b42b714292d3ea2e8de99b47
Danneyl Serna <114704164+AlterEgoSE@users.noreply.github.com>
2022-10-06 23:35:44 -0500

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿// Diseñar un programa para buscar y escribir la primera vocal leída del teclado.
+// (Se supone que se leen, uno a uno, caracteres desde el teclado.)
+
+char letra;
+
+Console.Write("Digite una letra: ");
+letra = char.Parse(Console.ReadLine());
+
+while (letra != 'a' && letra != 'e' && letra != 'i' && letra != 'o' && letra != 'u' && letra != 'A' && letra != 'E' && letra != 'I' && letra != 'O' && letra != 'U')
+{
+    Console.Write("Digite de nuevo: ");
+    letra = char.Parse(Console.ReadLine());
+}
+Console.ReadKey();
+
