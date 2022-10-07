137ae28fa2e55a92eb6323e6954c8a8c23a5556f
Danneyl Serna <114704164+AlterEgoSE@users.noreply.github.com>
2022-10-06 23:33:21 -0500

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿// Diseñar un programa que visualice el siguiente triángulo isósceles.
+int cant;
+Console.Write("Ingrese numero de filas de la piramide: ");
+cant = int.Parse(Console.ReadLine());
+for (int i = 1; i <= cant; i++)
+{
+    for (int j = 1; j <= cant - i; j++)
+    {
+        Console.Write(" ");
+    }
+    for (int k = 1; k <= i; k++)
+    {
+        Console.Write("*");
+    }
+    for (int l = 1; l <= i - 1; l++)
+    {
+        Console.Write("*");
+    }
+    Console.WriteLine();
+}
