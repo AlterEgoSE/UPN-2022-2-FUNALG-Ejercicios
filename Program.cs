388fb715994d8fb0d3473ad317fdcac4685f8e91
Danneyl Serna <114704164+AlterEgoSE@users.noreply.github.com>
2022-10-06 23:34:49 -0500

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿// Diseñar un programa para imprimir la suma de los primeros números naturales
+// impares menores o iguales que n.
+
+int num, suma = 0;
+
+Console.Write("Ingresa un número: ");
+num = int.Parse(Console.ReadLine());
+
+for (int i = 1; i <= num; i++)
+{
+    if (i%2 == 1)
+    {
+        suma = suma + i;
+    }
+}
+Console.WriteLine($"La suma de los números impares menores o igual a {num} es {suma}");
+Console.ReadKey();
