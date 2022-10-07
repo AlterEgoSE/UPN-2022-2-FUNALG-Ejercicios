63718e9bd0a0c10115a172d81a09b87a33ee043b
Danneyl Serna <114704164+AlterEgoSE@users.noreply.github.com>
2022-10-06 23:36:14 -0500

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿// Diseñar un programa que permita escribir en una pantalla la frase “¿Desea continuar? S/N”
+// hasta que la respuesta sea 'N'
+
+string respuesta;
+
+do
+{
+    Console.WriteLine("¿Desa continuar? Si / No: ");
+    respuesta = Console.ReadLine();
+}
+while (respuesta != "no" && respuesta != "NO");
+Console.ReadKey();
+
